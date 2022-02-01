bf79dfe01b0fb9dd23362ffbd54561991889ea44
Aleena <aleenabennyskpm@gmail.com>
2022-02-01 11:52:31 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-first_name = input("What is your name? ")
-print("Hello " + first_name)
-birth_year = input("Enter your birth year ")
-age = 2022 - int(birth_year)
-print("Your age is " + str(age))
-status = input("Are you a student? ")
-if status == "yes":
-    print(first_name + " is a student")
-else:
-    print(first_name + " is not a student")
-course = input("What are you studing? ")
-print("Oh! you are a " + course + " student")
-father = input("what is your father's name? ")
-print(father + " is " + first_name + "'s father")
-print(first_name + "'s full name is " + first_name+ father)
+
---
+++ calc.py
@@ -0,0 +1,4 @@
+number1=input("first:")
+number2=input("second:")
+sum= int(number1)+int(number2)
+print("sum=" +str(sum))
