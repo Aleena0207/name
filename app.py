0c86df3366e782ae768321cc1a14ca883a2cdfca
ALEENA BENNY <53312561+Aleena0207@users.noreply.github.com>
2022-02-01 12:02:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 
+first_name=input("What is your name? ")
+print("Hello " +first_name)
+birth_year=input("Enter your birth year ")
+age=2022-int(birth_year)
+print("Your age is "+str(age))
+status=input("Are you a student? ")
+if  status== "yes":
+ print( first_name +" is a student" )
+else:
+ print( first_name +" is not a student" )
+course=input("What are you studing? ")
+print( "Oh! you are a "+ course + " student" )
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
